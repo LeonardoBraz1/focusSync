1c1bdb2c47e3012d8cb1d343b5744e841ff881bc
LeonardoBraz1 <leosallvatore@gmail.com>
2023-06-27 11:44:58 -0300

    atualização

--- sistema/views/cadastro/Cargos.php
+++ sistema/views/cadastro/Cargos.php
@@ -88,32 +88,7 @@ include('../../components/head.php');
     <script type="text/javascript" src="../../assets/js/scripts/cadastro/editar.js"></script>
     <script type="text/javascript" src="../../assets/js/scripts/cadastro/deletar.js"></script>
     <script type="text/javascript" src="../../assets/js/scripts/cadastro/inserir.js"></script>
-    <!-- Google analytics script-->
-    <script type="text/javascript">
-        if (document.location.hostname == "pratikborsadiya.in") {
-            (function(i, s, o, g, r, a, m) {
-                i["GoogleAnalyticsObject"] = r;
-                (i[r] =
-                    i[r] ||
-                    function() {
-                        (i[r].q = i[r].q || []).push(arguments);
-                    }),
-                (i[r].l = 1 * new Date());
-                (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-                a.async = 1;
-                a.src = g;
-                m.parentNode.insertBefore(a, m);
-            })(
-                window,
-                document,
-                "script",
-                "//www.google-analytics.com/analytics.js",
-                "ga"
-            );
-            ga("create", "UA-72504830-1", "auto");
-            ga("send", "pageview");
-        }  
-    </script>
+    
 </body>
 
 </html>
